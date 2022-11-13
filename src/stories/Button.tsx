c79724b5e9f4421296683b397289b61ad71d3163
Chandrakant Pal <sanjay161097@gmail.com>
2022-11-14 02:38:35 +0530

    Different variants of buttons

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,48 +1,39 @@
-import React from 'react';
-import './button.css';
+import classNames from "classnames";
+import * as React from "react";
+import "./button.css";
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+export interface ButtonProps {
+  onClick: () => void;
+  variant?: "dark" | "light";
+  borderRadius?: "rounded" | "square" | "left-rounded" | "right-rounded";
+  leftIcon?: JSX.Element;
+  rightIcon?: JSX.Element;
+  text: string;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button: React.FC<ButtonProps> = ({
+  onClick,
+  borderRadius = "rounded",
+  variant = "light",
+  leftIcon,
+  rightIcon,
+  text,
+}) => {
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
+      onClick={onClick}
+      className={classNames("button", {
+        dark: variant === "dark",
+        square: borderRadius === "square",
+        "left-rounded": borderRadius === "left-rounded",
+        "right-rounded": borderRadius === "right-rounded",
+        "icon-left-spacing": leftIcon !== undefined,
+        "icon-right-spacing": rightIcon !== undefined,
+      })}
     >
-      {label}
+      {leftIcon}
+      <span>{text}</span>
+      {rightIcon}
     </button>
   );
 };
