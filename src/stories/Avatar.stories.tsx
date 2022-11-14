1de18a9102901263ced2a89c468d60c758ee2d87
Chandrakant Pal <sanjay161097@gmail.com>
2022-11-14 19:35:38 +0530

    argsType for children defined

--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -5,44 +5,32 @@ import { Avatar, AvatarProps } from "./Avatar";
 const meta: Meta = {
   title: "Avatar",
   component: Avatar,
+  argTypes: {
+    children: {
+      defaultValue: (
+        <img
+          src="/images/avatar1.png"
+          alt="avatar"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      ),
+    },
+  },
 };
 export default meta;
 
 const Template: Story<AvatarProps> = (args) => <Avatar {...args} />;
 
 export const DefaultSizeRegularAvatar = Template.bind({});
-DefaultSizeRegularAvatar.args = {
-  children: (
-    <img
-      src="/images/avatar1.png"
-      alt="avatar"
-      style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    />
-  ),
-};
 
 export const SmallSizeRegularAvatar = Template.bind({});
 SmallSizeRegularAvatar.args = {
   size: "small",
-  children: (
-    <img
-      src="/images/avatar1.png"
-      alt="avatar"
-      style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    />
-  ),
 };
 
 export const LargeSizeRegularAvatar = Template.bind({});
 LargeSizeRegularAvatar.args = {
   size: "large",
-  children: (
-    <img
-      src="/images/avatar1.png"
-      alt="avatar"
-      style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    />
-  ),
 };
 
 export const SamllSizeSquareAvatar = Template.bind({});
